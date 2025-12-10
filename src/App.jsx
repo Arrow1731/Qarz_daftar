0c08e3fd44bba609811bcaa3507151ee50161ac7
azizbek1701 <azizbekkamilov1701@gmail.com>
2025-12-10 12:07:05 +0500

    kghjgjgh

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import Lists from "./Pages/List_Qarz";
 import Profile from "./Pages/Profile_admin";
 import NotFound from "./Pages/NotFound";
 import Login from "./Pages/Login";
-import Register from "./Pages/register";
+// import Register from "./Pages/register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />  
-        <Route path="/list_qarz" element={<Lists />} />        
-        <Route path="/profile_admin" element={<Profile />} />
+        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/Home" element={<Home />} />  
+        <Route path="/List_Qarz" element={<Lists />} />        
+        <Route path="/Profile_admin" element={<Profile />} />
 
         {/* 404 page */}
         <Route path="*" element={<NotFound />} />
